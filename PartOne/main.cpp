adf5e6a0858d4818ed85817c6089dc730f7082a4
hue <huebert@gatech.edu>
2013-11-28 23:39:23 -0500

    Added comment header.

--- PartOne/main.cpp
+++ PartOne/main.cpp
@@ -1,9 +1,9 @@
-/*
- * main.cpp
- *
- *  Created on: Nov 28, 2013
- *      Author: hue
- */
+// CS3242 Operating Systems
+// Fall 2013
+// Project 6: Disks and File Systems
+// Richard L. Bailey
+// Date: 12/2/2013
+// File: main.cpp
 
 int main(void)
 {
